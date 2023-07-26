0f2e35b18a0251274e278d0f8a7f70cfd52c6264
Sarayu2323 <136216450+Sarayu2323@users.noreply.github.com>
2023-07-26 12:37:54 -0400

    Adds all the basic boilerplate for redux and all the handlers

--- src/App.js
+++ src/App.js
@@ -3,34 +3,67 @@ import './App.css';
 import { BOOK_LIST } from './books';
 import Book from './Book';
 import BookForm from './BookForm';
+import { actions } from './store';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
   const [bookList, setBookList] = useState(() => BOOK_LIST)
-  const [showBookForm, setShowBookForm] = useState(false)
+  const [showBookForm, setShowBookForm] = useState({ for: '', open: false })
+  const [selectedBook, setSelectedBook] = useState({})
+  const dispatch = useDispatch()
 
-  const handleDelete = (book) => {
+  const counter = useSelector((state) => state.counter)
+
+  const handleAddBook = (newBook) => {
+    setBookList([newBook, ...bookList])
+    setShowBookForm({ for: '', open: false })
+  }
+
+  const handleDeleteBook = (book) => {
     const newBookList = bookList.filter((b) => b.id !== book.id)
     setBookList(newBookList)
   }
 
-  const handleAddBook = (newBook) => {
-    setBookList([newBook, ...bookList])
-    setShowBookForm(false)
+  const handleEditBook = (book) => {
+    setSelectedBook(book)
   }
 
   return (
     <div>
-      <nav>Book Store</nav>
-      <button onClick={() => { if (!showBookForm) setShowBookForm(true) }} >Add Book</button>
+      <nav>
+        Book Store <h1>Counter: {counter}</h1>
+        <button
+          onClick={
+            () => {
+              dispatch(actions.increment())
+            }
+          }>Increment</button>
+        <button
+          onClick={() => {
+            if (!showBookForm.open) {
+              setShowBookForm({ for: 'add', open: true })
+            } 
+          }}
+        >Add Book</button>
+      </nav>
+      
       <ul>
         {
-          bookList.map((book) => <Book key={book.id} book={book} handleDelete={handleDelete} />)
+          bookList.map((book) => (
+            <Book
+              key={book.id}
+              book={book}
+              handleEditBook={handleEditBook}
+              handleDeleteBook={handleDeleteBook}
+            />
+          ))
         }
       </ul>
-      {showBookForm && (
+      {showBookForm.open && (
         <BookForm
           handleAddBook={handleAddBook}
           handleClose={() => { setShowBookForm(false) }}
+          bookDetails={selectedBook}
         />)
       }
     </div>
