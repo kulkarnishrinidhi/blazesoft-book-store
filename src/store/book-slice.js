61e5e1d6e7b8769041164d6931b1122db615a16f
Sarayu2323 <136216450+Sarayu2323@users.noreply.github.com>
2023-07-26 17:34:37 -0400

    Fixes typo

--- src/store/book-slice.js
+++ src/store/book-slice.js
@@ -3,20 +3,29 @@ import { BOOK_LIST } from "../constants/books";
 import { act } from "react-dom/test-utils";
 
 const bookStoreSlice = createSlice({
-    name: 'counter',
-    initialState: { bookList: BOOK_LIST },
-    reducers: {
-        addBook(state, action) {
-            const { newBook } = action.payload;
-            state.bookList = [newBook, ...state.bookList]
-        },
-        deleteBook(state, action) {
-            const { book } = action.payload
-            state.bookList = state.bookList.filter((b) => b.id !== book.id)
-        },
-    }
-})
+  name: "bookStore",
+  initialState: { bookList: BOOK_LIST },
+  reducers: {
+    addBook(state, action) {
+      const { newBook } = action.payload;
+      //check if new book or editing existing book
+      const foundIndex = state?.bookList.findIndex(
+        (elem) => elem.id === newBook.id
+      );
+      const isExistingBook = foundIndex > -1;
+      if (isExistingBook) {
+        state.bookList[foundIndex] = newBook;
+      } else {
+        state.bookList = [newBook, ...state.bookList];
+      }
+    },
+    deleteBook(state, action) {
+      const { book } = action.payload;
+      state.bookList = state.bookList.filter((b) => b.id !== book.id);
+    },
+  },
+});
 
 export const bookStoreActions = bookStoreSlice.actions;
 
-export default bookStoreSlice
+export default bookStoreSlice;
